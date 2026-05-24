5d29b396653bf8c9f83357e1fed7e47254c8bcca
Syahid Nurhidayatullah <syahidnursanjaya2005@gmail.com>
2026-05-24 18:45:49 +0700

    fix: update CORS to use FRONTEND_URL env variable

--- fullstack/frontend/src/services/api.js
+++ fullstack/frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://capstoneproject-production-f10d.up.railway.app/api',
 })
 
 export const predict = (data) => api.post('/predict', data, {
