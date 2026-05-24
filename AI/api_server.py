5fbd522fedad704ef83069414035e3cf2d4a5b9a
_ngaabby <idogabriel2@gmail.com>
2026-05-24 20:33:53 +0700

    fixing requirement & add dinamic port in api_server

--- AI/api_server.py
+++ AI/api_server.py
@@ -187,4 +187,7 @@ def recommend(request: PredictionRequest):
         )
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
+
